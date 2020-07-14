8679d5e79923771645fdb8b4f4755ae5b91fbf55
Scott Ganyo <scott@ganyo.com>
2020-07-14 09:20:07 -0700

    update internal deps to latest

--- cmd/provision/provision.go
+++ cmd/provision/provision.go
@@ -407,15 +407,21 @@ func (p *provision) printConfig(config *server.Config, printf shared.FormatFn, v
 			Bytes: x509.MarshalPKCS1PrivateKey(config.Tenant.PrivateKey)})
 
 		// create CRD for secret
-		kidProp := fmt.Sprintf(server.SecretKIDFormat, config.Tenant.PrivateKeyID)
 		jwksBytes, err := json.Marshal(config.Tenant.JWKS)
 		if err != nil {
 			return err
 		}
+
+		props := map[string]string{server.SecretPropsKIDKey: config.Tenant.PrivateKeyID}
+		propsBuf := new(bytes.Buffer)
+		if err := server.WriteProperties(propsBuf, props); err != nil {
+			return err
+		}
+
 		secretData := map[string]string{
 			server.SecretJKWSKey:    base64.StdEncoding.EncodeToString(jwksBytes),
 			server.SecretPrivateKey: base64.StdEncoding.EncodeToString(privateKeyBytes),
-			server.SecretKIDKey:     base64.StdEncoding.EncodeToString([]byte(kidProp)),
+			server.SecretPropsKey:   base64.StdEncoding.EncodeToString(propsBuf.Bytes()),
 		}
 
 		secretCRD := server.SecretCRD{
--- go.mod
+++ go.mod
@@ -7,7 +7,7 @@ go 1.13
 // replace github.com/apigee/apigee-remote-service-envoy => ../apigee-remote-service-envoy
 
 require (
-	github.com/apigee/apigee-remote-service-envoy v1.0.0-beta.4
+	github.com/apigee/apigee-remote-service-envoy v1.0.0-beta.4.0.20200714144941-97b5b78e6934
 	github.com/apigee/apigee-remote-service-golib v1.0.0-beta.4
 	github.com/bgentry/go-netrc v0.0.0-20140422174119-9fd32a8b3d3d
 	github.com/google/go-querystring v1.0.0
